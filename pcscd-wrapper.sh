25eeb7a1d770fe3e0c054a4984dfd6344218647d
Evan Jarrett <evan@evanjarrett.com>
2025-08-15 01:21:00 -0500

    fix wrapper

--- pcscd-wrapper.sh
+++ pcscd-wrapper.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+echo "Running wrapper entrypoint"
 pcscd &
 sleep 2
 chmod 666 /run/pcscd/pcscd.comm 2>/dev/null || true
+
+ps -f | cat
 
 # Execute original entrypoint if it existed, otherwise run passed commands
 exec /usr/local/bin/docker-entrypoint.sh.orig "$@"
